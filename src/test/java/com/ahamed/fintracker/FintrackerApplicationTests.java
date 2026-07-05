ada43f728fe84c0333a32835e4a3625725976747
ahamed-sha-11 <iahamedsha@gmail.com>
2026-07-05 10:33:48 +0530

    Initial commit: Spring Boot base finance tracking pet project

---
+++ src/test/java/com/ahamed/fintracker/FintrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahamed.fintracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
